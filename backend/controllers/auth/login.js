9d2114791221002566c691e6d3bd88c2de0b5bb3
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-05-28 11:25:19 -0400

    [Add] local Storage

--- backend/controllers/auth/login.js
+++ backend/controllers/auth/login.js
@@ -34,6 +34,7 @@ const login = async (req = request, res = response) => {
       sameSite: 'none',
       secure: true,
     });
+    localStorage.setItem('authToken', token);
 
     return res.status(200).json({
       token,
