3b3e1975bd09b5aebff46a1e986c27023b679e1f
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-06-08 15:56:45 -0500

    whatsapp modified

--- frontend/src/components/Page/Layout/index.jsx
+++ frontend/src/components/Page/Layout/index.jsx
@@ -32,7 +32,7 @@ export function Layout({ children, image, title }) {
         <div className='dark:text-black z-50 absolute'>
         <WhatsAppWidget
           CompanyIcon={ClubIconSVG}
-          phoneNumber="584149056161"
+          phoneNumber="5491125142747"
           companyName="Club Agronomía Central"
           replyTimeText="Usualmente responde en un día"
           message="¡Hola! ¿Cómo puedo ayudarte?"
